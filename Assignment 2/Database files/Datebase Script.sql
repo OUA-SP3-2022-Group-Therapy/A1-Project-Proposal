a28ec27a3987f16569447eb74edca8f379cc0b99
JoshWags <101152131+JoshWags@users.noreply.github.com>
2022-10-11 08:58:37 +1100

    Some changes from yesterday

--- Assignment 2/Database files/Datebase Script.sql
+++ Assignment 2/Database files/Datebase Script.sql
@@ -1,76 +1,77 @@
 --This script was used to create the database. Please refer to [query file] for the database queries.
 --All account info is stored here.
 CREATE TABLE Account
-        (Email VARCHAR(100) NOT NULL,
+        (UserID VARCHAR(10) NOT NULL,
+        Email VARCHAR(100) NOT NULL,
         Password VARCHAR(100) NOT NULL,
         Name VARCHAR(20),
         User_Type VARCHAR(20),
         Gender VARCHAR(20),
         Date_Created DATE,
-        PRIMARY KEY (Email, Password)
+        PRIMARY KEY (UserID)
         );
 
 --Family group info is stored here
 CREATE TABLE Family_Group
-        (GroupName VARCHAR(100) NOT NULL,
+        (GroupID VARCHAR(10) NOT NULL,
         FamilyName VARCHAR(100) NOT NULL,
         DateCreated DATE,
         MemberStatus VARCHAR(20),
-        PRIMARY KEY (GroupName)
+        PRIMARY KEY (GroupID)
         );
 
 --Membership to family group links is stored here
 CREATE TABLE Membership
-        (Email VARCHAR(100) NOT NULL,
-        Password VARCHAR(100) NOT NULL,
-        GroupName VARCHAR(100) NOT NULL,
-        PRIMARY KEY (Email, Password, GroupName)
-        FOREIGN KEY (Email, Password) REFERENCES Account(Email, Password)
-        FOREIGN KEY (GroupName) REFERENCES Family_Group(GroupName)
+        (UserID VARCHAR(10) NOT NULL,
+        GroupID VARCHAR(10) NOT NULL,
+        PRIMARY KEY (UserID, GroupID)
+        FOREIGN KEY (UserID) REFERENCES Account(UserID)
+        FOREIGN KEY (GroupID) REFERENCES Family_Group(GroupID)
         );
 
 --Schedule info is stored here
 CREATE TABLE Schedule
-        (ScheduleName VARCHAR(100) NOT NULL,
-        GroupName VARCHAR(100) NOT NULL,
+        (ScheduleID VARCHAR(10) NOT NULL,
+        ScheduleName VARCHAR(100),
         StartDateTime DATETIME,
         EndDateTime DATETIME,
         Type VARCHAR(20),
         Frequency VARCHAR(20),
         Portion_Dose VARCHAR(20),
         Description VARCHAR(300),
-        PRIMARY KEY (ScheduleName)
-        FOREIGN KEY (GroupName) REFERENCES Family_Group(GroupName)
+        GroupID VARCHAR(10),
+        PRIMARY KEY (ScheduleID)
+        FOREIGN KEY (GroupID) REFERENCES Family_Group(GroupID)
         );
 
 --Event info is stored here
 CREATE TABLE Event
-        (ScheduleName VARCHAR(100) NOT NULL,
+        (ScheduleID VARCHAR(10) NOT NULL,
         EventName VARCHAR(100) NOT NULL,
         StartDateTime DATETIME,
         EndDateTime DATETIME,
         EventStatus INTEGER,
         CompletedBy VARCHAR(100),
-        PRIMARY KEY (ScheduleName, EventName)
-        FOREIGN KEY (ScheduleName) REFERENCES Schedule(ScheduleName)
+        PRIMARY KEY (ScheduleID, EventName)
+        FOREIGN KEY (ScheduleID) REFERENCES Schedule(ScheduleID)
         );
 
 --Pet info is stored here
 CREATE TABLE Pet
-        (PetID VARCHAR(15) NOT NULL,
+        (PetID VARCHAR(10) NOT NULL,
         Name VARCHAR(100) NOT NULL,
         Species VARCHAR(100),
         Breed VARCHAR(100),
         DOB DATE,
         Allergies VARCHAR(300),
-        GroupName VARCHAR(100),
+        GroupID VARCHAR(10),
         PRIMARY KEY (PetID)
-        FOREIGN KEY (GroupName) REFERENCES Family_Group(GroupName)
+        FOREIGN KEY (GroupID) REFERENCES Family_Group(GroupID)
         );
         
 --Pet Trend info is stored here
 CREATE TABLE Trends
-        (PetID VARCHAR(15) NOT NULL,
+        (PetID VARCHAR(10) NOT NULL,
         Date DATE NOT NULL,
         Height INTEGER,
         Weight INTEGER,
