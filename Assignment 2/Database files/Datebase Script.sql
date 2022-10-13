e0354013c7edecc19751c2a64bfe13d5a7b8cb20
JoshWags <101152131+JoshWags@users.noreply.github.com>
2022-10-13 18:08:40 +1100

    Draft query script

--- Assignment 2/Database files/Datebase Script.sql
+++ Assignment 2/Database files/Datebase Script.sql
@@ -3,7 +3,7 @@
 CREATE TABLE Account
         (UserID VARCHAR(10) NOT NULL,
         Email VARCHAR(100) NOT NULL,
-        Pass VARCHAR(25) NOT NULL,
+        Password VARCHAR(25) NOT NULL,
         Name VARCHAR(20),
         User_Type VARCHAR(20),
         Gender VARCHAR(20),
--- Assignment 2/Database files/Query Script.sql
+++ Assignment 2/Database files/Query Script.sql
@@ -1,24 +1,115 @@
 --Verify login request [Unfinished]
-SELECT UserID, Email, Pass
-FROM Account;
+SELECT UserID, Email, Password
+FROM Account
+WHERE Email = /*Email*/ and Password = /*Password*/;
 
---Signup initial
-INSERT INTO Account (UserID, Email, Pass, Date_Created)
-VALUES (/*RANDINT*/, /*User input 1*/, /*User input 2*/, DATETIME('now');
+--Signup add initial
+INSERT INTO Account (UserID, Email, Password, Date_Created)
+VALUES (/*RANDINT*/, /*User_input*/, /*User_input2*/, DATETIME('now'));
+--Need to create a trigger than also edits this in the Membershipp table
 
---Signup secondary
+--Signup add secondary
 INSERT INTO Account (User_Type, Gender)
 VALUES (/*User_Type*/, /*Gender*/)
+WHERE USERID = /*UserID*/
 
---Group details/Dashboard retrieve
+--Modify user details
+INSERT INTO Account (Email, Password, User_Type, Gender, Date_Created)
+VALUES (/*VALUES*/)
+WHERE USERID = /*UserID*/
+
+--Retrieve user details
+SELECT *
+FROM Account
+WHERE UserID = /*UserID*/;
+
+--Delete user details;
+DELETE FROM Account (Email, Password, User_Type, Gender, Date_Created)
+Where UserID = /*UserID*/;
+--Need to delete it from Membership as well
+
+--Add group details
+INSERT INTO Family_Group (GroupID, FamilyName, DateCreated, MemberStatus)
+VALUES (/*VALUES*/);
+
+--Modify group details
+INSERT INTO Family_Group (FamilyName, DateCreated, MemberStatus)
+VALUES (/*VALUES*/)
+WHERE GroupID = /*GroupID*/;
+
+--Retrieve group details
 SELECT *
 FROM Family_Group
-WHERE GroupID = /*Selected ID*/
+WHERE GroupID = /*Selected ID*/;
 
---Family Group/Dashboard part 2 retrieve
+--Delete group details
+DELETE FROM Family_Group (FamilyName, DateCreated, MemberStatus)
+Where GroupID = /*Selected ID*/;
 
---User Profile
+--Add schedule details
+INSERT INTO Schedule (ScheduleID, ScheduleName StartDateTime, EndDateTime, Type, Frequency, Portion/dose, Description)
+VALUES (/*VALUES*/);
 
---Pet Profile
+--Modify schedule details
+INSERT INTO Schedule (ScheduleName StartDateTime, EndDateTime, Type, Frequency, Portion/dose, Description)
+VALUES (/*VALUES*/)
+WHERE ScheduleID = /*ScheduleID*/;
 
---Add/Edit Schedule
+--Retrieve schedule details
+SELECT *
+FROM Schedule
+WHERE ScheduleID = /*ScheduleID*/;
+
+--Delete schedule details
+DELETE FROM Schedule (ScheduleName StartDateTime, EndDateTime, Type, Frequency, Portion/dose, Description)
+Where ScheduleID = /*ScheduleID*/;
+
+--Add event details
+INSERT INTO Event (ScheduleID, EventName, StartDateTime, EndDateTime, EventStatus, CompletedBy)
+VALUES (/*VALUES*/);
+
+--Modify event details
+INSERT INTO Event (EventName, StartDateTime, EndDateTime, EventStatus, CompletedBy)
+VALUES (/*VALUES*/)
+WHERE ScheduleID = /*ScheduleID*/ and EventName = /*EventName*/;
+
+--Modify event details (Event for checking off)
+INSERT INTO Event (EventStatus, CompletedBy)
+VALUES (/*VALUES*/)
+WHERE ScheduleID = /*ScheduleID*/ and EventName = /*EventName*/;
+
+--Retrieve event details
+SELECT *
+FROM Event
+WHERE ScheduleID = /*ScheduleID*/ and EventName = /*EventName*/;
+
+--Delete event details
+DELETE FROM INSERT INTO Event (EventName, StartDateTime, EndDateTime, EventStatus, CompletedBy)
+WHERE ScheduleID = /*ScheduleID*/ and EventName = /*EventName*/;
+
+--Add pet details
+INSERT INTO Pet (PetID, Name, Species, Breed, DOB, Allergies)
+VALUES (/*VALUES*/);
+
+--Modify pet details
+INSERT INTO Pet (Name, Species, Breed, DOB, Allergies)
+VALUES (/*VALUES*/)
+WHERE PetID = /*PetID*/;
+
+--Retrieve pet details
+SELECT *
+FROM Pet
+WHERE PetID = /*PetID*/;
+
+--Delete pet details
+DELETE FROM INSERT INTO Pet (Name, Species, Breed, DOB, Allergies)
+WHERE PetID = /*PetID*/;
+
+--Add pet trend entry
+INSERT INTO Trends (PetID, Date, Height, Weight)
+VALUES (/*VALUES*/)
+
+--Retrieve pet trend entry
+SELECT *
+FROM Trends
+WHERE PetID = /*PetID*/ and Date = /*Date*/;
